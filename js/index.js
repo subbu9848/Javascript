a197a895218134510fb254bd7b967ff19aa0784a
subbu9848 <manya.aithal@gmail.com>
2022-03-23 16:11:59 +0530

    second commit

---
+++ js/index.js
@@ -0,0 +1,101 @@
+// 1 ways to print in Javascript
+// console.log("Hello World");
+// alert("me");
+// document.write ("this is document write")
+
+// 2. Javascript console API
+// console.log("Hello World", 4 + 6, "Another log");
+// console.warn("This is warning");
+// console.error("This is Error");
+
+// 3 Javascript variables
+//  what is variables ? Containers to store data values
+
+/*
+var number1 = 36;
+var number2 = 40;*/
+
+// console.log(number1 + number2);
+
+// console.log(number1 - number2);
+
+// console.log(number1 * number2);
+
+// console.log(number1 / number2);
+
+// 4 Data Types in Javascript
+// numbers
+
+// var num1 = 455;
+// var num2 = 56.76;
+
+// string
+// var num1 = "this is a string";
+// var num1 = "this is also a string";
+
+// objects
+
+/*var marks = {
+  ravi: 34,
+  subham: 78,
+  subbu: 80,
+};
+*/
+
+// console.log(num2);
+
+// console.log(marks);
+
+// 5 Booleans
+
+// var a = true;
+// var b = false;
+
+// console.log(a, b);
+
+// var und;
+
+// console.log(und);
+
+// var n = null;
+
+// console.log(n);
+
+/*
+At a very high level, there are two types of data types in Java script 
+
+1 primitive data types: undefined, null, number, string boolean, symbol
+
+2 Refrence data types: Arrays and objects
+
+*/
+
+// var arr = [1, 2, "subbu", 4, 5];
+
+// console.log(arr[4]);
+// console.log(arr);
+
+// operators in Java script
+
+// arthemtics
+// var a = 34;
+// var b = 56;
+
+// console.log("The value of a+b is", a + b);
+// console.log("The value of a+b is", a - b);
+// console.log("The value of a+b is", a * b);
+// console.log("The value of a+b is", a / b);
+
+// Assigment operators
+
+// var c = b;
+// c += 2; c= c-2
+// console.log(c);
+
+// var x = 34;
+// var y = 56;
+
+// // console.log(x == y);
+
+// console.log(x >= y);
+// console.log(y >= x);
